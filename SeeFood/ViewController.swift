8860a1bc07a0126027e54ae324362b57841021f7
Khaled Bohout <khaledbohout2012@gmail.com>
2019-06-15 21:09:04 +0200

    Final Project Working

--- SeeFood/ViewController.swift
+++ SeeFood/ViewController.swift
@@ -7,12 +7,69 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate {
 
+
+    @IBOutlet weak var imageView: UIImageView!
+    let imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = false
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            
+        imageView.image = userPickedImage
+            
+            guard let ciimage = CIImage(image: userPickedImage) else {
+                fatalError("could not convert to CIImage")
+            }
+            detect(image: ciimage)
+            
+        }
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    func detect(image: CIImage) {
+        
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {
+            fatalError("loading coreML model failed")
+        }
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            guard let result = request.results as? [VNClassificationObservation] else {
+                fatalError("model failed to process image")
+            }
+            if let firstresult = result.first {
+                
+                if firstresult.identifier.contains("hotdog") {
+                    self.navigationItem.title = "HotDog"
+                }
+                else {
+                    self.navigationItem.title = "Not HotDog!"
+                }
+            }
+        }
+        let handler = VNImageRequestHandler(ciImage: image)
+        do {
+            try handler.perform([request])
+        } catch {
+            print(error)
+        }
+    
+    }
+    
+    @IBAction func cameraTapped(_ sender: Any) {
+        
+        present(imagePicker, animated: true,completion: nil)
     }
 
 
